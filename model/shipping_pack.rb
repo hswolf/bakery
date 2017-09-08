6842888f04dbcde0c6b4fdedd77e78c73818eab0
VinhTo <vinhtt0305@gmail.com>
2017-09-08 21:37:15 +0900

    Output is minimum number of pack.

--- model/shipping_pack.rb
+++ model/shipping_pack.rb
@@ -7,24 +7,47 @@ class ShippingPack
   end
 
   def self.create_shipping_packs(order_quantity, packs)
-    array_of_possible_quantities_per_pack = packs.map do |pack|
-      generate_possible_quantities_per_pack(order_quantity, pack)
-    end
-
+    sorted_packs = packs.sort_by { |pack| pack.number_of_unit }
+    x = min_number_of_pack(sorted_packs.size - 1, order_quantity, sorted_packs)
     binding.pry
   end
 
-  def self.generate_possible_quantities_per_pack(order_quantity, pack)
-    possible_quantities = [0]
-    quantity = 1
+  def self.min_number_of_pack(last, quantity, packs)
+    return 0 if quantity == 0
 
-    loop do
-      possible_quantities << quantity
-
-      break if quantity * pack.number_of_unit >= order_quantity
-      quantity += 1
+    if last == 0
+      if quantity % packs[last].number_of_unit == 0
+        return quantity / packs[last].number_of_unit
+      else
+        return false
+      end
     end
 
-    possible_quantities
+    excluded_last = min_number_of_pack(last - 1, quantity, packs)
+
+    quantity_after = quantity - (quantity / packs[last].number_of_unit) * packs[last].number_of_unit
+    included_last = begin
+        (quantity / packs[last].number_of_unit) + min_number_of_pack(last - 1, quantity_after, packs)
+      rescue TypeError
+        false
+      end
+
+    [excluded_last, included_last].select{|num| num}.min
+  end
+end
+
+class ShippingPacks
+  attr_accessor :total_unit_number, :shipping_packs, :total_price
+
+  def add_pack(shipping_pack)
+    @shipping_packs << shipping_pack
+  end
+
+  def total_unit_number
+    @shipping_packs.sum { |shipping_pack| shipping_pack.quantity * shipping_pack.pack.number_of_unit }
+  end
+
+  def total_price
+    @shipping_packs.sum { |shipping_pack| shipping_pack.quantity * shipping_pack.pack.price}
   end
 end
