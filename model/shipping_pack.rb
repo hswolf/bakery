5ca05b29701744f6621cf9b61d27acc8cf345478
Vinh To <vinhtt0305@gmail.com>
2017-09-09 17:28:03 +0900

    Add logic for comparing different solutions by price.

--- model/shipping_pack.rb
+++ model/shipping_pack.rb
@@ -1,60 +1,97 @@
 class ShippingPack
-  attr_accessor :quantity, :pack
+  attr_accessor :pack, :count
 
-  def initialize(quantity, pack)
-    @quantity = quantity
+  def initialize(pack, count)
     @pack = pack
+    @count = count
   end
 
   def self.create_shipping_packs(order_quantity, packs)
     sorted_packs = packs.sort_by { |pack| pack.number_of_unit }
     @@saved_array = {}
-    best_shipping_packs(sorted_packs.size - 1, order_quantity, sorted_packs)
-    # binding.pry
+    x = best_shipping_packs(sorted_packs.size - 1, order_quantity, sorted_packs)
   end
 
   private
 
   def self.best_shipping_packs(last, quantity, packs)
-    if quantity <= 0
-      return 0
-    end
+    return 0 if quantity <= 0
 
-    if last == 0
-      if quantity % packs[last].number_of_unit == 0
-        return new(quantity / packs[last].number_of_unit, packs[last])
-      else
-        return false
-      end
-    end
+    return new(packs[last], quantity / packs[last].number_of_unit) if last == 0
 
     saved_key = "#{last}|#{quantity}".to_sym
 
     if @@saved_array[saved_key].nil?
       quotient = quantity / packs[last].number_of_unit
 
-      possible_shipping_pack_arrays = (0..quotient).map do |i|
+      possible_packages = (0..quotient).map do |i|
         new_quantity = quantity - i * packs[last].number_of_unit
-        shipping_pack = new(i, packs[last])
+        shipping_pack = new(packs[last], i)
 
         [shipping_pack, best_shipping_packs(last - 1, new_quantity, packs)].flatten - [0]
       end
 
-      @@saved_array[saved_key] = select_min_in_shipping_pack_arrays(possible_shipping_pack_arrays)
+      @@saved_array[saved_key] = best_packages(possible_packages, quantity)
     end
 
     return @@saved_array[saved_key]
   end
 
-  def self.select_min_in_shipping_pack_arrays(arrays)
-    return [] if arrays.empty?
+  def self.best_packages(packages, quantity)
+    return [] if packages.empty?
 
-    arrays.select { |arr|  arr && !arr.include?(false) && !arr.include?(nil) }
-      .sort { |first, second| total_pack_of_shipping_packs(first) <=> total_pack_of_shipping_packs(second) }
+    packages.select { |package| package && !package.include?(false) && !package.include?(nil) }
+      .sort { |first, second| Package.new(first, quantity) <=> Package.new(second, quantity) }
       .first
   end
+end
 
-  def self.total_pack_of_shipping_packs(shipping_packs)
-    shipping_packs.sum { |shipping_pack| shipping_pack.quantity }
+class Package
+  attr_accessor :shipping_packs, :demanded_quantity
+
+  def initialize(shipping_packs, demanded_quantity)
+    @shipping_packs = shipping_packs
+    @demanded_quantity = demanded_quantity
+  end
+
+  def total_numb_of_pack
+    self.shipping_packs.sum { |shipping_pack| shipping_pack.count }
+  end
+
+  def total_numb_of_unit
+    self.shipping_packs.sum { |shipping_pack| shipping_pack.count * shipping_pack.pack.number_of_unit }
+  end
+
+  def total_price
+    self.shipping_packs.sum { |shipping_pack| shipping_pack.count * shipping_pack.pack.price }
+  end
+
+  def different_to_demanded_quantity
+    (total_numb_of_unit - self.demanded_quantity).abs
+  end
+
+  def <=>(another_package)
+    return 0 if self.demanded_quantity != another_package.demanded_quantity
+
+
+    if different_to_demanded_quantity < another_package.different_to_demanded_quantity
+      return -1
+    elsif different_to_demanded_quantity > another_package.different_to_demanded_quantity
+      return 1
+    end
+
+    if total_numb_of_pack < another_package.total_numb_of_pack
+      return -1
+    elsif total_numb_of_pack > another_package.total_numb_of_pack
+      return 1
+    end
+
+    if total_price < another_package.total_price
+      return -1
+    elsif total_price > another_package.total_price
+      return 1
+    end
+
+    return 0
   end
 end
